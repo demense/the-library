e5571cbbefab3d5dfc0506664ac00a953ed7894e
Ahmed <ahmedadil9102@gmail.com>
2024-10-22 13:23:37 +0300

    Everything working fine

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// An array to store book objects
 let library = [];
 
 // Book object constructor function
@@ -16,89 +17,98 @@ function Book(title, author, pages, readStatus) {
     });
 }
 
-// A function to a add a book to the library
+// A function to create a book instance and add it to the library
 function addToLibrary(title, author, pages, readStatus) {
   let bookObj = new Book(title, author, pages, readStatus);
   library.push(bookObj);
 }
 
-addToLibrary("s", "s", "23", "Yes");
-addToLibrary("ps", "3s", "223", "No");
-addToLibrary("dps", "s", "23", "Yes");
-addToLibrary("mdps", "3s", "223", "No");
+// A function to display the last added book in the library
+function displayBook() {
+  const book = document.createElement("div");
+  book.className = "book";
+  book.dataset.id = library.length - 1;
+  document.querySelector(".content").appendChild(book);
 
-function displayBooks() {
-  for (let i = 0; i < library.length; i++) {
-    const book = document.createElement("div");
-    book.className = "book";
-    book.dataset.id = i;
-    document.querySelector(".content").appendChild(book);
+  const info = document.createElement("div");
+  info.className = "info";
+  book.appendChild(info);
 
-    const info = document.createElement("div");
-    info.className = "info";
-    book.appendChild(info);
+  const title = document.createElement("p");
+  title.classList = "title";
+  title.textContent = `Title: ${library.at(-1).title}`;
+  info.appendChild(title);
 
-    const title = document.createElement("p");
-    title.classList = "title";
-    title.textContent = `Title: ${library.at(i).title}`;
-    info.appendChild(title);
+  const author = document.createElement("p");
+  author.classList = "author";
+  author.textContent = `Author: ${library.at(-1).author}`;
+  info.appendChild(author);
 
-    const author = document.createElement("p");
-    author.classList = "author";
-    author.textContent = `Author: ${library.at(i).author}`;
-    info.appendChild(author);
+  const pages = document.createElement("p");
+  pages.classList = "pages";
+  pages.textContent = `Pages: ${library.at(-1).pages}`;
+  info.appendChild(pages);
 
-    const pages = document.createElement("p");
-    pages.classList = "pages";
-    pages.textContent = `Pages: ${library.at(i).pages}`;
-    info.appendChild(pages);
+  const readStatus = document.createElement("p");
+  readStatus.classList = "readStatus";
+  readStatus.textContent = `Read: ${library.at(-1).readStatus}`;
+  info.appendChild(readStatus);
 
-    const readStatus = document.createElement("p");
-    readStatus.classList = "readStatus";
-    readStatus.textContent = `Read: ${library.at(i).readStatus}`;
-    info.appendChild(readStatus);
+  const buttons = document.createElement("div");
+  buttons.className = "book-buttons";
+  buttons.style.cssText = " display: flex; gap: 8px; ";
+  book.appendChild(buttons);
 
-    const buttons = document.createElement("div");
-    buttons.className = "book-buttons";
-    buttons.style.cssText = " display: flex; gap: 8px; ";
-    book.appendChild(buttons);
+  const toggle = document.createElement("button");
+  toggle.className = "toggle-read-status";
+  toggle.textContent = "Toggle read status";
+  buttons.appendChild(toggle);
 
-    const toggle = document.createElement("button");
-    toggle.className = "toggle-read-status";
-    toggle.textContent = "Toggle read status";
-    buttons.appendChild(toggle);
+  const remove = document.createElement("button");
+  remove.className = "remove";
+  remove.textContent = "Remove";
+  buttons.appendChild(remove);
 
-    const remove = document.createElement("button");
-    remove.className = "remove";
-    remove.textContent = "Remove";
-    buttons.appendChild(remove);
+  toggle.addEventListener("click", () => {
+    library[book.dataset.id].changeReadStatus();
+    readStatus.textContent = `Read: ${library[book.dataset.id].readStatus}`;
+    console.log(library[book.dataset.id]);
+  });
 
-    toggle.addEventListener("click", () => {
-      library[book.dataset.id].changeReadStatus();
-      readStatus.textContent = `Read: ${
-        library[book.dataset.id].readStatus
-      }`;
-      console.log(library[book.dataset.id]);
-    });
+  remove.addEventListener("click", () => {
+    library.splice(book.dataset.id, 1);
+    book.remove();
+    let books = document.querySelectorAll(".book");
+    for (let prop in books) {
+      books[prop].dataset.id = prop;
+    }
+  });
 
-    remove.addEventListener("click", () => {
-      library.splice(book.dataset.id, 1);
-      book.remove();
-      let books = document.querySelectorAll(".book");
-      for (let prop in books) {
-        books[prop].dataset.id = prop;
-      }
-    });
-  }
+  // The html structure for every book
+  // <div class="book" data-id="">
+  //   <div class="info">
+  //     <p class="title">Title: </p>
+  //     <p class="author">Author: </p>
+  //     <p class="pages">Pages: </p>
+  //     <p class="readStatus">Read: </p>
+  //   </div>
+  //   <div class="buttons">
+  //     <button type="button" class="toggle-read-status">
+  //       Toggle read status
+  //     </button>
+  //     <button type="button" class="remove">
+  //       Remove
+  //     </button>
+  //   </div>
+  // </div>
 }
-displayBooks();
 
 // Add event listener to "Add a book" button
 document.querySelector(".add-book").addEventListener("click", () => {
   // Open form modal
   document.querySelector("dialog").showModal();
 
-  // Clear form input fields
+  // Clear form input fields for new input
   document.querySelector("#title").value = "";
   document.querySelector("#author").value = "";
   document.querySelector("#pages").value = "";
@@ -116,7 +126,7 @@ document.querySelector(".submit").addEventListener("click", (event) => {
   // Prevent submit button default behavior
   event.preventDefault();
 
-  // collect book info from form input
+  // collect book info from the form inputs
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
   let pages = document.querySelector("#pages").value;
@@ -125,43 +135,9 @@ document.querySelector(".submit").addEventListener("click", (event) => {
   // add the book to the library
   addToLibrary(title, author, pages, readStatus);
 
-  console.log(library);
-
   // close form modal
   document.querySelector("dialog").close();
 
+  // Display the book
   displayBook();
 });
-
-// function displayBooks() {
-//   for (let i = 0; i < library.length; i++) {
-//     const book = document.createElement("div");
-//     book.className = "book";
-//     document.querySelector('.content').appendChild(book);
-
-//     const info = document.createElement("div");
-//     info.className = "info";
-//     book.appendChild(info);
-
-//     const title = document.createElement("p");
-//     title.classList = "title";
-//     title.textContent = `Title: ${library.at(i).title}`;
-//     info.appendChild(title);
-
-//     const author = document.createElement("p");
-//     author.classList = "author";
-//     author.textContent = `Author: ${library.at(i).author}`;
-//     info.appendChild(author);
-
-//     const pages = document.createElement("p");
-//     pages.classList = "pages";
-//     pages.textContent = `Pages: ${library.at(i).pages}`;
-//     info.appendChild(pages);
-
-//     const readStatus = document.createElement("p");
-//     readStatus.classList = "readStatus";
-//     readStatus.textContent = `Read: ${library.at(i).readStatus}`;
-//     info.appendChild(readStatus);
-
-//   }
-// }
